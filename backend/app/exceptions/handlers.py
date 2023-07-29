d9ecc40857b99e2c3f06aac58c18d838c5a686ed
sunny <stratdn@mail.ru>
2023-07-29 16:44:27 +0400

    fix: исправлены exception handlers

--- backend/app/exceptions/handlers.py
+++ backend/app/exceptions/handlers.py
@@ -1,10 +1,13 @@
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy.exc import IntegrityError
 from starlette import status
 from starlette.responses import JSONResponse
-from starlette.exceptions import HTTPException
 
-from exceptions.custom_excaptions import UserAlreadyExists, AuthenticationError, \
-    NotAuthenticatedError
+from exceptions.custom_excaptions import (
+    UserAlreadyExists,
+    AuthenticationError,
+    NotAuthenticatedError,
+)
 
 
 async def user_already_exists_error_handler(_, exc: UserAlreadyExists) -> JSONResponse:
@@ -12,9 +15,14 @@ async def user_already_exists_error_handler(_, exc: UserAlreadyExists) -> JSONRe
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder(
             {
-                "result": False,
-                "error_type": "User already exists",
-                "error_message": "USER ALREADY EXISTS",
+                {
+                    "detail": [
+                        {
+                            "type": "USER ALREADY EXISTS",
+                            "msg": "user with this username already exists",
+                        }
+                    ]
+                }
             }
         ),
     )
@@ -25,10 +33,15 @@ async def authentication_error_handler(_, exc: AuthenticationError) -> JSONRespo
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder(
             {
-                "result": False,
-                "error_type": "AUTHENTICATION ERROR",
-                "error_message": "USERNAME AND PASSWORD DO NOT MATCH "
-                                 "OR USERNAME DOES NOT EXIST",
+                {
+                    "detail": [
+                        {
+                            "type": "AUTHENTICATION ERROR",
+                            "msg": "username and password do not match "
+                            "or username does not exists",
+                        }
+                    ]
+                }
             }
         ),
     )
@@ -38,10 +51,13 @@ async def not_authenticated_error(_, exc: NotAuthenticatedError):
     return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
         content=jsonable_encoder(
-            {
-                "result": False,
-                "error_type": " 401 UNAUTHORIZED",
-                "error_message": "authentication failed"
-            }
+            {"detail": [{"type": "401 UNAUTHORIZED", "msg": "authentication failed"}]}
         ),
+    )
+
+
+async def integrity_error_handler(_, exc: IntegrityError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"detail": "data base integrity error"}),
     )
--- backend/app/init_app.py
+++ backend/app/init_app.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from sqlalchemy.exc import IntegrityError
 
 from api.v1.api_routers import api_router
 from api.v1.endpoints.auth import auth_router
 from exceptions import handlers
 from configs import app_configs
-from exceptions.custom_excaptions import UserAlreadyExists, AuthenticationError, \
-    NotAuthenticatedError
+from exceptions.custom_excaptions import (
+    UserAlreadyExists,
+    AuthenticationError,
+    NotAuthenticatedError,
+)
 
 
 def create_app() -> FastAPI:
@@ -15,15 +19,19 @@ def create_app() -> FastAPI:
 
     app.add_exception_handler(
         exc_class_or_status_code=UserAlreadyExists,
-        handler=handlers.user_already_exists_error_handler
+        handler=handlers.user_already_exists_error_handler,
     )
     app.add_exception_handler(
         exc_class_or_status_code=AuthenticationError,
-        handler=handlers.authentication_error_handler
+        handler=handlers.authentication_error_handler,
     )
     app.add_exception_handler(
         exc_class_or_status_code=NotAuthenticatedError,
-        handler=handlers.not_authenticated_error
+        handler=handlers.not_authenticated_error,
+    )
+    app.add_exception_handler(
+        exc_class_or_status_code=IntegrityError,
+        handler=handlers.integrity_error_handler,
     )
 
     return app
